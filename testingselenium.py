51e7326f55c414eaa56285ad6a2b32dd3702f7aa
D3stros <patrick.fallmann@hotmail.com>
2020-06-12 23:44:11 +0200

    Automating google search

--- testingselenium.py
+++ testingselenium.py
@@ -1,4 +1,11 @@
 from selenium import webdriver
+import time
 
 driver = webdriver.Chrome()
-driver.get('https://www.google.com')
+driver.get('https://www.google.com/xhtml')
+time.sleep(5)
+search_field = driver.find_element_by_name('q')
+search_field.send_keys('google')
+search_field.submit()
+time.sleep(5)
+driver.quit()
