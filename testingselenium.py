4c73090d59ccf3e6ca4105ed8ce5b3d361a7c541
D3stros <patrick.fallmann@hotmail.com>
2020-06-13 15:48:44 +0200

    Using implicit waiting to give instructions on how long driver should wait

--- testingselenium.py
+++ testingselenium.py
@@ -6,12 +6,7 @@ from selenium.webdriver.support import expected_conditions as ExpectedCondition
 from selenium.webdriver.common.by import By
 
 driver = webdriver.Chrome()
+driver.implicitly_wait(3)
 driver.get('https://www.google.com/xhtml')
-try:
-    element = WebDriverWait(driver, 5).until(
-        ExpectedCondition.presence_of_element_located((By.NAME, "quh"))
-    )
-    element.send_keys("google")
-finally:
-    driver.quit()
+myelement = driver.find_element_by_name('quh')
 
