58556e9cdc93a35a76ee0b78e7eee8f99f3ed554
D3stros <patrick.fallmann@hotmail.com>
2020-06-13 15:13:53 +0200

    Finding elements via xpath

--- testingselenium.py
+++ testingselenium.py
@@ -4,7 +4,7 @@ import time
 driver = webdriver.Chrome()
 driver.get('https://www.google.com/xhtml')
 time.sleep(5)
-search_field = driver.find_element_by_name('q')
+search_field = driver.find_element_by_xpath("//input[@name=q]")
 search_field.send_keys('google')
 search_field.submit()
 time.sleep(5)
