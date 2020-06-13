7110f1caab613b1b36f7ea5a8274543e8bd33d3f
D3stros <patrick.fallmann@hotmail.com>
2020-06-13 15:46:35 +0200

    Testing performance

--- testingselenium.py
+++ testingselenium.py
@@ -1,11 +1,17 @@
 from selenium import webdriver
 import time
 
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as ExpectedCondition
+from selenium.webdriver.common.by import By
+
 driver = webdriver.Chrome()
 driver.get('https://www.google.com/xhtml')
-time.sleep(5)
-search_field = driver.find_element_by_xpath("//input[@name=q][@type='text']")
-search_field.send_keys('google')
-search_field.submit()
-time.sleep(5)
-driver.quit()
+try:
+    element = WebDriverWait(driver, 5).until(
+        ExpectedCondition.presence_of_element_located((By.NAME, "quh"))
+    )
+    element.send_keys("google")
+finally:
+    driver.quit()
+
