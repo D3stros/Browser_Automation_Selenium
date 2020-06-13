ef59a5e05104368859d7ab1cfda569bc48da9961
D3stros <patrick.fallmann@hotmail.com>
2020-06-13 14:28:56 +0200

    Going backward and forward in the browser history

--- moreonselenium.py
+++ moreonselenium.py
@@ -5,9 +5,8 @@ from selenium.webdriver.common.keys import Keys
 
 driver = webdriver.Chrome()
 driver.get('https://www.google.com/xhtml')
-search_field = driver.find_elements_by_class_name('gLFyf')
-for i in search_field:
-    if i.tag_name == "input":
-        i.send_keys('google', Keys.ARROW_LEFT, Keys.BACKSPACE, Keys.ENTER)
-        break
+driver.get('https://www.youtube.com')
+driver.back()
+driver.forward()
+
 
