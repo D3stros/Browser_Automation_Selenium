51f1677db9b6b0455ec9939deb00f3a8356fa73b
D3stros <patrick.fallmann@hotmail.com>
2020-06-13 13:37:03 +0200

    Checking with a for loop if element is an input

--- moreonselenium.py
+++ moreonselenium.py
@@ -3,7 +3,10 @@ import time
 
 driver = webdriver.Chrome()
 driver.get('https://www.google.com/xhtml')
-search_field = driver.find_element_by_class_name('gLFyf')
-search_field.send_keys('google')
-search_field.submit()
-driver.quit()
+search_field = driver.find_elements_by_class_name('gLFyf')
+for i in search_field:
+    if i.tag_name == "input":
+        i.send_keys('google')
+        i.submit()
+        break
+
