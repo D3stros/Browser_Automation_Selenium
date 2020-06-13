e877bddf35c8155180afc6ff3a1c5988c55a52ee
D3stros <patrick.fallmann@hotmail.com>
2020-06-13 14:47:37 +0200

    Adding and printing cookies

--- moreonselenium.py
+++ moreonselenium.py
@@ -5,8 +5,12 @@ from selenium.webdriver.common.keys import Keys
 
 driver = webdriver.Chrome()
 driver.get('https://www.google.com/xhtml')
-driver.get('https://www.youtube.com')
-driver.back()
-driver.forward()
+cookie = {'name': 'token', 'value': '4asdasdasdasddas'}
+driver.add_cookie(cookie)
+driver.get('https://www.google.com')
+print(driver.get_cookie('token'))
+print(driver.get_cookies())
+
+
 
 
