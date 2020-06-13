04b268daeb1796478f6c926545b030a47ae361ad
D3stros <patrick.fallmann@hotmail.com>
2020-06-13 15:59:20 +0200

    Creating own waiting condition has_css_class

--- testingselenium.py
+++ testingselenium.py
@@ -5,8 +5,26 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ExpectedCondition
 from selenium.webdriver.common.by import By
 
+class has_css_class(object):
+    def __init__(self, locator, css):
+        self.locator = locator
+        self.css = css
+
+    def __call__(self, driver):
+        element = driver.find_element(*self.locator)
+        if self.css in element.get_attribute("class"):
+            return element
+        else:
+            return False
+
 driver = webdriver.Chrome()
-driver.implicitly_wait(3)
 driver.get('https://www.google.com/xhtml')
-myelement = driver.find_element_by_name('quh')
+try:
+    element = WebDriverWait(driver, 5).until(
+        has_css_class((By.NAME, "q"), "gsfi")
+    )
+    element.send_keys("google")
+    time.sleep(3)
+finally:
+    driver.quit()
 
